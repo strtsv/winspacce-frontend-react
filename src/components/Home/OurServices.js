cf6ffdd537ecdc93e945da37d9da341ea84b920c
jsusilo <enr@bk.ru>
2021-01-22 00:22:00 +0500

    Home OurServices Comp

---
+++ src/components/Home/OurServices.js
@@ -0,0 +1,125 @@
+import React from "react";
+
+class OurServices extends React.Component {
+  render() {
+    return (
+      <section className="section section-sm">
+        <div className="container">
+          <h3 className="text-center">Our services</h3>
+          <div className="row row-30">
+            <div className="col-sm-6 col-lg-4">
+              <div className="box-icon-moderm">
+                <div className="icon nc-icon-outline shopping_stock" />
+                <div className="box-icon-moderm-body">
+                  <h5 className="box-icon-moderm-title">
+                    <a href="#">WINDOW FRAME REPAIR</a>
+                  </h5>
+                  <p className="box-icon-moderm-text">
+                    If your window frame requires some repair, we'll gladly fix
+                    everything as quick as possible.
+                  </p>
+                  <a className="box-icon-link" href="#">
+                    View Details
+                    <span className="nc-icon-mini arrows-1_minimal-right" />
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col-sm-6 col-lg-4">
+              <div className="box-icon-moderm">
+                <div className="icon nc-icon-outline ui-1_settings" />
+                <div className="box-icon-moderm-body">
+                  <h5 className="box-icon-moderm-title">
+                    <a href="#">WINDOW INSTALLATION AND REPAIR</a>
+                  </h5>
+                  <p className="box-icon-moderm-text">
+                    We do all the work from measuring to installation. You only
+                    need to select the window type.
+                  </p>
+                  <a className="box-icon-link" href="#">
+                    View Details
+                    <span className="nc-icon-mini arrows-1_minimal-right" />
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col-sm-6 col-lg-4">
+              <div className="box-icon-moderm">
+                <div className="icon nc-icon-outline users_parent" />
+                <div className="box-icon-moderm-body">
+                  <h5 className="box-icon-moderm-title">
+                    <a href="#">WINDOW SHUTTER INSTALLATION</a>
+                  </h5>
+                  <p className="box-icon-moderm-text">
+                    When you want to save your room from too strong sun, we can
+                    offer you a solution.
+                  </p>
+                  <a className="box-icon-link" href="#">
+                    View Details
+                    <span className="nc-icon-mini arrows-1_minimal-right" />
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col-sm-6 col-lg-4">
+              <div className="box-icon-moderm">
+                <div className="icon nc-icon-outline business_math" />
+                <div className="box-icon-moderm-body">
+                  <h5 className="box-icon-moderm-title">
+                    <a href="#">REPLACEMENT WINDOW SHAPES</a>
+                  </h5>
+                  <p className="box-icon-moderm-text">
+                    We offer new window shapes as sometimes the old ones need to
+                    be replaced or improved.
+                  </p>
+                  <a className="box-icon-link" href="#">
+                    View Details
+                    <span className="nc-icon-mini arrows-1_minimal-right" />
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col-sm-6 col-lg-4">
+              <div className="box-icon-moderm">
+                <div className="icon nc-icon-outline ui-1_home-minimal" />
+                <div className="box-icon-moderm-body">
+                  <h5 className="box-icon-moderm-title">
+                    <a href="#">SECONDARY DOUBLE GLAZING</a>
+                  </h5>
+                  <p className="box-icon-moderm-text">
+                    This option gives additional protection from hazardous
+                    weather and helps to keep warm in cold period.
+                  </p>
+                  <a className="box-icon-link" href="#">
+                    View Details
+                    <span className="nc-icon-mini arrows-1_minimal-right" />
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col-sm-6 col-lg-4">
+              <div className="box-icon-moderm">
+                <div className="icon nc-icon-outline furniture_door" />
+                <div className="box-icon-moderm-body">
+                  <h5 className="box-icon-moderm-title">
+                    <a href="#">BI-FOLDING DOORS</a>
+                  </h5>
+                  <p className="box-icon-moderm-text">
+                    Such shape of the doors helps to save the room space. We use
+                    super strong glass and fastening systems of high quality.
+                  </p>
+                  <a className="box-icon-link" href="#">
+                    View Details
+                    <span className="nc-icon-mini arrows-1_minimal-right" />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default OurServices;
